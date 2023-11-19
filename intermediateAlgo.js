800cec7d86d126718c472b2c301789ae2736012b
ArjayBuen <arjaybuen10@gmail.com>
2023-11-20 00:59:52 +0800

    continuation learning algo

--- intermediateAlgo.js
+++ intermediateAlgo.js
@@ -11,3 +11,28 @@ function sumAll(arr) {
   sumAll([1, 4]);
   sumAll([5, 10]) //should return 45
   sumAll([10, 5]) //should return 45
+
+
+  function diffArray(arr1, arr2) {
+    const newArr = [];
+    function onlyInFirst(first, second) {
+      // Looping through an array to find elements that don't exist in another array
+      for (let i = 0; i < first.length; i++) {
+        if (second.indexOf(first[i]) === -1) {
+          // Pushing the elements unique to first to newArr
+          newArr.push(first[i]);
+        }
+      }
+    }
+  
+    onlyInFirst(arr1, arr2);
+    onlyInFirst(arr2, arr1);
+    return newArr;
+  }
+  
+  diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
+  [1, "calf", 3, "piglet"], [1, "calf", 3, 4] //should return ["piglet", 4]
+  diffArray([], ["snuffleupagus", "cookie monster", "elmo"]); 
+  //should return ["snuffleupagus", "cookie monster", "elmo"]
+  [1, "calf", 3, "piglet"], [7, "filly"] 
+  //should return [1, "calf", 3, "piglet", 7, "filly"]
